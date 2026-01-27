bb0d57d99fb1824aac4ca63affc70d5527ce12e3
ruben-bena <181500053+ruben-bena@users.noreply.github.com>
2026-01-27 19:14:19 +0100

    Finished CheatCommand

--- src/commands/cheatCommand.js
+++ src/commands/cheatCommand.js
@@ -1,7 +1,10 @@
 import AbstractCommand from './abstractCommand.js'
 
 export default class CheatCommand extends AbstractCommand {
-    execute() {
+    execute(game) {
         console.log("Esto es un CheatCommand")
+        console.log(`Antes del comando: cheatsEnabled=${game.cheatsEnabled}`)
+        game.cheatsEnabled = !game.cheatsEnabled
+        console.log(`Después del comando: cheatsEnabled=${game.cheatsEnabled}`)
     }
 }
--- src/game/game.js
+++ src/game/game.js
@@ -13,7 +13,7 @@ export default class Game {
     }
 
     execute(command) {
-        command.execute()
+        command.execute(this)
     }
 
     toJSON() {
