83470b615fe251df880553a8f33d773cadfe65f3
ruben-bena <181500053+ruben-bena@users.noreply.github.com>
2026-03-04 16:55:51 +0100

    Implemented class CheatCommand

--- src/commands/cheatCommand.js
+++ src/commands/cheatCommand.js
@@ -2,9 +2,9 @@ import AbstractCommand from './abstractCommand.js'
 
 export default class CheatCommand extends AbstractCommand {
     execute(game) {
-        console.log("Esto es un CheatCommand")
-        console.log(`Antes del comando: cheatsEnabled=${game.cheatsEnabled}`)
         game.cheatsEnabled = !game.cheatsEnabled
-        console.log(`Después del comando: cheatsEnabled=${game.cheatsEnabled}`)
+        console.log()
+        console.log(`***** cheatsEnabled=${game.cheatsEnabled} *****`)
+        console.log()
     }
 }
