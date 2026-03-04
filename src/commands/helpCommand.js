8e669cdf1fc036ffca3fe2dde9d84d0ad58f48c1
ruben-bena <181500053+ruben-bena@users.noreply.github.com>
2026-03-04 16:52:30 +0100

    Implemented class HelpCommand

--- src/commands/helpCommand.js
+++ src/commands/helpCommand.js
@@ -2,6 +2,28 @@ import AbstractCommand from './abstractCommand.js'
 
 export default class HelpCommand extends AbstractCommand {
     execute() {
-        console.log("Esto es un HelpCommand")
+        console.log("\n=================================================");
+        console.log("                   📖  AYUDA  📖");
+        console.log("=================================================\n");
+
+        console.log("  🆘  help | ajuda");
+        console.log("      → Muestra la lista de comandos.\n");
+
+        console.log("  📂  carregar partida 'nom_arxiu.json'");
+        console.log("      → Carga una partida guardada.\n");
+
+        console.log("  💾  guardar partida 'nom_guardar.json'");
+        console.log("      → Guarda la partida actual.\n");
+
+        console.log("  🕵️  trampes | cheats");
+        console.log("      → Muestra u oculta el tablero con las casillas destapadas.\n");
+
+        console.log("  🎯  XY   (ej: B3)");
+        console.log("      → Destapa la casilla e indica la distancia al tesoro más cercano.\n");
+
+        console.log("  🏆  puntuacio | score");
+        console.log("      → Muestra la puntuación y las tiradas restantes.\n");
+
+        console.log("=================================================\n");
     }
 }
